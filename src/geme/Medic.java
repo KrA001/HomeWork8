607059bda85adf3c49b590f43f2a71444cfd7d72
LENOVO <test23062000@gmail.com>
2023-10-05 16:54:42 +0300

    HLS

--- src/geme/Medic.java
+++ src/geme/Medic.java
@@ -11,7 +11,7 @@ public class Medic extends Hero {
         for (int i = 0; i < heroes.length; i++) {
             if (!heroes[i].getName().equals(this.getName()) && this.getHealth() > 0 && heroes[i].getHealth() > 0 && heroes[i].getHealth() < 100) {
                 heroes[i].setHealth(heroes[i].getHealth() + 10);
-                System.out.println(getName() + " ВЫЛЕЧИЛ " + heroes[i].getName());
+                System.out.println(getName() + " HEALTH " + heroes[i].getName());
                 break;
             }
         }
